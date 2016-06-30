17f264a52d137e136e38420e4b2b964a17902285
jvargas <jvargas@cj.com>
2016-06-29 20:17:56 -0700

    rooms

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,67 @@
-module Lib
-  ( run
-  ) where
+module Lib where
+
+import Control.Applicative
+import Data.Attoparsec.Text
+import Data.Text (Text)
+
+data Cmd
+  = CmdAct Act
+  | CmdOpt Opt
+  deriving (Show, Eq)
+
+data Act
+  = ActMove Loc
+  deriving (Show, Eq)
+
+data Opt
+  = OptQuit
+  deriving (Show, Eq)
+
+data Pack = Pack
+
+data Loc
+  = LocBasement
+  | LocAttic
+  | LocStaircase
+  | LocDiningRoom
+  | LocLivingRoom
+  | LocOffice
+  | LocGreenRoom
+  | LocBlueRoom
+  | LocRedRoom
+  | LocDarkRoom
+  deriving (Show, Eq)
+
+data Item = Item
+
+data Ch = Ch
+
+data Obj = Obj
+
+runPar :: Parser a -> Text -> Maybe a
+runPar par text = maybeResult $ parse par text
+
+tryCvt :: Text -> a -> Parser a
+tryCvt text loc = try $ asciiCI text *> pure loc
+
+cmdPar :: Parser Cmd
+cmdPar = CmdAct <$> movePar
+
+movePar :: Parser Act
+movePar = asciiCI "move" *> many1 space *> (ActMove <$> locPar)
+
+locPar :: Parser Loc
+locPar =
+  (tryCvt "basement" LocBasement) <|>
+  (tryCvt "attic" LocAttic) <|>
+  (tryCvt "staircase" LocStaircase) <|>
+  (tryCvt "dining-room" LocDiningRoom) <|>
+  (tryCvt "living-room" LocLivingRoom) <|>
+  (tryCvt "office" LocOffice) <|>
+  (tryCvt "blue-room" LocBlueRoom) <|>
+  (tryCvt "red-room" LocRedRoom) <|>
+  (tryCvt "green-room" LocGreenRoom) <|>
+  (tryCvt "dark-room" LocDarkRoom)
 
 run :: IO ()
-run = do
-  return ()
+run = return ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import Test.Hspec
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "parse" $ do
+    it "basement" $ do
+      shouldBe
+        (runPar locPar "basement")
+        (Just LocBasement)
+    it "move" $ do
+      shouldBe
+        (runPar cmdPar "move basement")
+        (Just $ CmdAct (ActMove LocBasement))
